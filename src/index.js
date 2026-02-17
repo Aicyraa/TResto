8a96a5f7e8a7bdb8345b36aa1c87bf6fcfad9bcd
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-02-17 20:36:42 +0800

    added favicon working with index.js (handles switching tab logic)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 // Handles logic for content loading
-import "./style/home.css"
+import "./style/main.css"
 import { branch } from "./contents/branches.js"
 import { home } from "./contents/home.js"
 import { menu } from "./contents/menu.js"
 
-alert(branch)
-alert(home)
-alert(menu)
+const el = {
+   nav: document.querySelector("nav"),
+   content: document.querySelector("main"),
+
+}
+
+function switchTab(e) {
+   const tabs = ["home", "menu", "branches"]
+   const target = e.target;
+   if (tabs.includes(target.id)) {
+      switch(target.id) {
+         
+      }   
+   }
+   
+}
+
+
+function loadTab(currentTab) {
+   
+}
+
+(function(){
+   el.nav.addEventListener("click", switchTab) // switch tab function
+
+})();
