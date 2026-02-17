0ddfc0b0555b4c5dc505904199cd2761605ada10
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-02-17 17:16:31 +0800

    test alert

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Handles logic for content loading
+import "./style/home.css"
+import { branch } from "./contents/branches.js"
+import { home } from "./contents/home.js"
+import { menu } from "./contents/menu.js"
+
+alert(branch)
+alert(home)
+alert(menu)
